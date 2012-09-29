b49c8004bcadebf94641adcc8c8d55fa8d2b261e
Koray Göksu <korayg2000@gmail.com>
2012-09-29 12:56:25 +0300

    Source Codes
    Source S

---
+++ src/com/github/koray0505/ctf/CTF.java
@@ -0,0 +1,16 @@
+package com.github.koray0505.ctf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CTF extends JavaPlugin {
+	
+    @Override
+    public void onEnable(){
+        // TODO ** Mineqolikin ilk plugini acildi **
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO ** Mineqolikin ilk plugini kapandi **
+    }
+}
